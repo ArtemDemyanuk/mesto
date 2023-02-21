c178e1ec63c659fe1cff6e549bcc797aec2cd039
Artem Demyanuk <a.demyanuk@yandex.ru>
2023-02-21 21:44:58 +0300

    Mesto

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+const profileName = document.querySelector(".profile__name");
+const profileAbout = document.querySelector(".profile__about");
+const userNameInput = document.querySelector(".popup__input_type_name");
+const profileAboutInput = document.querySelector(".popup__input_type_about");
+
+function editinProfileName(evt) {
+
+  evt.preventDefault();
+
+  profileName.textContent = userNameInput.value;
+  profileAbout.textContent = profileAboutInput.value;
+
+  closePopup();
+}
+
+const openEditedPopup = document.querySelector(".profile__edit-button");
+const popupContainer = document.querySelector(".popup");
+const popupButtonClose = document.querySelector(".popup__close");
+const submitButton = document.querySelector(".popup__button");
+
+
+function openPopup() {
+  userNameInput.value = profileName.textContent;
+  profileAboutInput.value = profileAbout.textContent;
+  popupContainer.classList.add("popup_opened")
+}
+
+function closePopup() {
+  popupContainer.classList.remove("popup_opened")
+}
+
+openEditedPopup.addEventListener("click", openPopup);
+popupButtonClose.addEventListener("click", closePopup);
+submitButton.addEventListener("click", editinProfileName);
